729041ef6a2f102dfd6aa2327723559d2cc0a289
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-11-22 01:05:29 +0200

    Started localstorage implementation

--- magic.js
+++ magic.js
@@ -1,15 +1,4 @@
 
-
-
-
-
-
-function modifyWeight(w, adjust_value_from_button){
-    w.currentWeight += adjust_value_from_button;
-    document.getElementsByClassName(w.htmlClassName+" paino")[0].innerHTML = w.currentWeight + w.unit;
-    return;
-}
-
 let painot = {
 
     penkki: { 
@@ -17,7 +6,6 @@ let painot = {
         currentWeight: 57.50,
         unit: "kg",
         htmlClassName: "penkkipunnerrus",
-        adjustWeight:modifyWeight
    },
 
     talja:  { 
@@ -25,14 +13,39 @@ let painot = {
         currentWeight: 40.00,
         unit: "kg", 
         htmlClassName: "ylätalja",
-        adjustedWeight:modifyWeight
    }
 }
 
+if (localStorage.getItem("painot_ls") === null) {
+ // De nada, do nothing
+ localStorage.setItem('painot_ls', JSON.stringify(painot));
+}
+else {
+    // Retrieve stored weights
+    let painot_ls = JSON.parse(window.localStorage.getItem("painot_ls"));
+    console.log(painot_ls);
 
-// window.onload = function(){
-//         
-//
-// document.getElementsByClassName(painot.penkki.htmlClassName+" paino")[0].innerHTML = painot.penkki.currentWeight;
-//     
-// }
+};
+
+
+
+function modifyWeight(w, adjust_value_from_button){
+    w.currentWeight += adjust_value_from_button;
+    document.getElementsByClassName(w.htmlClassName+" paino")[0].innerHTML = w.currentWeight + w.unit;
+    
+    getLocalStorateWeights();
+    return;
+}
+
+
+function getLocalStorateWeights() {
+    for (const laite in painot) {
+        console.log("laite: " +laite); // laite, talja
+        const laiteObj = painot[laite];
+        console.log("laiteObj: "+laiteObj);
+        for (const muuttuja_arvo in laiteObj) { // itse key/value
+            console.log("laiteObj[muuttuja_arvo]: "+laiteObj[muuttuja_arvo]);
+
+    }
+  }
+};
