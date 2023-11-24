0d6479bf86d2956bf05e27961fb2c8ae42e947a3
Tommi Ollila <138831117+TomG-FIN@users.noreply.github.com>
2023-11-24 02:50:29 +0200

    Release V0.01 Alpha

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # kuntosali-apuri
-Nettisivu joka auttaa muistamaan mitä painoja käytit viimeksi
+#### Nettisivu joka auttaa muistamaan mitä painoja käytit viimeksi kuntosalilla tehdessäsi suoritetta.
+
+**v0.01 Alpha - 2023-11-24:**
+- Muistaa laittamasi painot per nettiselain käyttämällä LocalStoragea
+- Tällä hetkellä ainoastaan penkkipunnerrus, sekä ylätalja käytettävissä
+
+TODO:
+- Luodaan dynaamisesti sisältö JavaScriptillä
+- Etusivu jossa muutama painike, esim. "yläkroppa" & "jalkapäivä"
+- Kahden napin ajastin sarjojen välille
--- magic.js
+++ magic.js
@@ -1,4 +1,4 @@
-
+// Current database of weights
 let painot = {
 
     penkki: { 
@@ -16,27 +16,21 @@ let painot = {
    }
 }
 
-if (localStorage.getItem("painot_ls") === null) {
- // De nada, do nothing
- localStorage.setItem('painot_ls', JSON.stringify(painot));
-}
-else {
-    // Retrieve stored weights
-    let painot_ls = JSON.parse(window.localStorage.getItem("painot_ls"));
-    console.log(painot_ls);
-
-};
 
 
 
-function modifyWeight(w, adjust_value_from_button){
-    w.currentWeight += adjust_value_from_button;
-    document.getElementsByClassName(w.htmlClassName+" paino")[0].innerHTML = w.currentWeight + w.unit;
-    
-    getLocalStorateWeights();
+
+function modifyWeight(w){
+    adjust_value_from_button = parseFloat(w.dataset.weight); // Store weight amount from data-weight custom attribute in HTML5
+    let dataname =  w.parentNode.parentElement.classList[0] // "penkkipunnerrus" traversal aka get class name
+
+    tehtäväkohtainen_lista = searchValue(painot, dataname) // Returns basically object list under "paino"
+
+    painot[tehtäväkohtainen_lista].currentWeight += adjust_value_from_button; 
+    document.getElementsByClassName(painot[tehtäväkohtainen_lista].htmlClassName+" paino")[0].innerHTML = painot[tehtäväkohtainen_lista].currentWeight + painot[tehtäväkohtainen_lista].unit;
+    localStorage.setItem('painot_ls', JSON.stringify(painot)); // Save to local storage
     return;
 }
-
 
 function getLocalStorateWeights() {
     for (const laite in painot) {
@@ -49,3 +43,49 @@ function getLocalStorateWeights() {
     }
   }
 };
+
+function searchValue(obj, targetValue) {
+    for (const key in obj) {
+      if (obj[key] === targetValue) {
+        // If the current key's value matches the target value, return the parent object
+        return obj;
+      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+        // If the current key's value is an object, recursively search within that object
+        const result = searchValue(obj[key], targetValue);
+        if (result) {
+          return key;
+        }
+      }
+    }
+    // If the target value is not found in the current object, return null
+    return null;
+}
+
+document.addEventListener('readystatechange', event => { 
+
+  // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
+  if (event.target.readyState === "complete") {
+
+    if (localStorage.getItem("painot_ls") === null) { // If no local storage cache exists
+      localStorage.setItem('painot_ls', JSON.stringify(painot));
+     }
+
+     else { // If cache exists, download the file
+         // Retrieve stored weights
+         painot = JSON.parse(window.localStorage.getItem("painot_ls"));
+         console.log(painot);
+     };
+
+     for (const key in painot){
+
+      document.getElementsByClassName(painot[key].htmlClassName+" paino")[0].innerHTML = painot[key].currentWeight + painot[key].unit;
+     
+    }
+
+
+
+
+
+
+  }
+});
